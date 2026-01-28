ff33bd475ea215cb8dacff47d527446faef72582
samod <83113845+BugSlayer9000@users.noreply.github.com>
2026-01-28 17:35:05 +0000

    Added : NavBar component with functional toggle switch for Light mode and dark mode

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,13 @@
-import { Box, Button } from "@chakra-ui/react";
+import { Box, Button, useColorModeValue } from "@chakra-ui/react";
 import { Route, Routes } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import CreatePage from "./pages/CreatePage";
 import Navbar from "./components/Navbar";
 
+
 function App() {
   return (
-    <Box>
+    <Box minH={"100vh"} bg={useColorModeValue("gray.100", "gray.900")}>
       <Navbar/>
       <Routes>
         <Route path="/" element={<HomePage />} />
--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,11 +1,55 @@
-import React from 'react'
+import {
+  Container,
+  Flex,
+  Text,
+  HStack,
+  Button,
+  useColorMode,
+} from "@chakra-ui/react";
+import React from "react";
+import { Link } from "react-router-dom";
+import { FaPlus } from "react-icons/fa";
+
+import { FaSun } from "react-icons/fa";
+import { FaMoon } from "react-icons/fa";
 
 const Navbar = () => {
-  return (
-    <div>
-      navbar
-    </div>
-  )
-}
+  const { colorMode, toggleColorMode } = useColorMode();
 
-export default Navbar
+  return (
+    <Container maxW={"1140px"} px={4}>
+      <Flex
+        h={16}
+        alignItems={"center"}
+        justifyContent={"space-between"}
+        flexDir={{
+          base: "column",
+          sm: "row",
+        }}
+      >
+        <Text
+          bgGradient={"linear(to-r, cyan.400, blue.500)"}
+          bgClip={"text"}
+          fontSize={{ base: "22", sm: "28" }}
+          fontWeight="Bold"
+          textAlign={"center"}
+          textTransform={"uppercase"}
+        >
+          <Link to={"/"}>Product Store 🛒</Link>
+        </Text>
+        <HStack spacing={2} alignItems={"center"}>
+          <Link to={"/create"}>
+            <Button>
+              <FaPlus  fontSize={20}/>
+            </Button>
+          </Link>
+          <Button onClick={toggleColorMode}>
+            {colorMode === "light" ? <FaMoon /> : <FaSun />}
+          </Button>
+        </HStack>
+      </Flex>
+    </Container>
+  );
+};
+
+export default Navbar;
