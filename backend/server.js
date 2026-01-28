01815806f4c94bf6e6360bbc3c8e68b17566e1d2
samod <83113845+BugSlayer9000@users.noreply.github.com>
2026-01-28 12:15:11 +0000

    - added final touches to the backend

--- backend/server.js
+++ backend/server.js
@@ -9,12 +9,13 @@ import productRoute from "./routes/product.route.js";
 dotenv.config();
 
 const app = express();
+const PORT = process.env.PORT || 5000 ;
 
 app.use(express.json()); // allows us to accept json data in the body
 
 app.use("/api/products", productRoute);
 
-app.listen(5000, () => {
+app.listen(PORT, () => {
   connectDB();
   console.log("Server started at http://localhost:5000");
 });
