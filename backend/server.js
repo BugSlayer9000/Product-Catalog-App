c345200a2de6311d607a711bdaff378fe734a677
samod <83113845+BugSlayer9000@users.noreply.github.com>
2026-02-05 12:50:56 +0000

    Added : Update server configuration and package dependencies for improved environment management

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import { connectDB } from "./config/db.js";
-
+import path from "path"
 import productRoute from "./routes/product.route.js";
 
 dotenv.config();
@@ -11,9 +11,19 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000 ;
 
+const __dirname = path.resolve()
+
 app.use(express.json()); // allows us to accept json data in the body
 
 app.use("/api/products", productRoute);
+
+if(process.env.NODE_ENV === "production"){
+  app.use(express.static(path.join(__dirname,"/frontend/dist")))
+
+  app.get(/.*/,(req,res) =>{
+    res.sendFile(path.resolve(__dirname,"../frontend/dist/index.html"))
+  })
+}
 
 app.listen(PORT, () => {
   connectDB();
